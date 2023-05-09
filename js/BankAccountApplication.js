ff90a2ed7a386525720b709d80dcf4e8725e9239
Raimundo Pereira <raipereira84@gmail.com>
2023-05-09 14:09:08 -0500

    edit lab 7

--- js/BankAccountApplication.js
+++ js/BankAccountApplication.js
@@ -15,7 +15,6 @@ class BankAccount{
 
     deposit(amount) {
         this._balance += amount;
-       alert(this.balance);
     }
 
     debit(amount) {
@@ -26,23 +25,28 @@ class BankAccount{
         }
       }
 
+      static renderAccounts(){
+        document.getElementById("account-list").value ='';
+        this.accountInfoList.forEach((account) =>{
+            document.getElementById('account-list').value += `Account name: ${account.name}\nBalance: ${account.balance}\n\n`;
+        })
+      }
+
     static createAccount(){
         const name = document.getElementById("name").value;
         const inicialDesposit = parseFloat(document.getElementById("deposit").value);
         const account = new BankAccount(name, inicialDesposit);
         this.accountInfoList.push(account);
-        document.getElementById("account-list").value ='';
-        this.accountInfoList.forEach((account) =>{
-            document.getElementById('account-list').value += `Account name: ${account.name}\nBalance: ${account.balance}\n\n`;
-        })
-        this.updateAccountDropdown();
+        this.renderAccounts()
+        this.updateAccountDropdownDeposit();
+        this.updateAccountDropdownDepit();
     }
 
-    static updateAccountDropdown() {
+    static updateAccountDropdownDeposit() {
         const dropdown = document.getElementById('list-account');
         dropdown.innerHTML = '';
         const option = document.createElement('option');
-        option.value='Select Account';
+        option.value='';
         option.textContent = 'Select Account';
         dropdown.appendChild(option);
         this.accountInfoList.forEach((account) => {
@@ -53,17 +57,45 @@ class BankAccount{
         });
       }
 
+      static updateAccountDropdownDepit() {
+        const dropdown = document.getElementById('list-account-depit');
+        dropdown.innerHTML = '';
+        const option = document.createElement('option');
+        option.value='';
+        option.textContent = 'Select Account';
+        dropdown.appendChild(option);
+        this.accountInfoList.forEach((account) => {
+          const option = document.createElement('option');
+          option.value = account.balance;
+          option.textContent = account.name;
+          dropdown.appendChild(option);
+        });
+      }
+
+      static operation_depit() {
+        const optionAmount = parseFloat(document.getElementById('amount-depit').value);
+        const optionElement = document.getElementById('list-account-depit');
+        const optionName = optionElement.options[optionElement.selectedIndex];
+        const acc = BankAccount.accountInfoList.filter((acc) => {
+            if(acc._name == optionName.innerHTML){
+                return acc;
+            }
+        });
+        acc[0].debit(optionAmount);
+        this.renderAccounts()
+      }
+
       static operation_deposit() {
-       
-       // const amount_deposit = parseFloat(document.getElementById('amount').value);
-        const optionAmount = parseFloat(document.getElementById('list-account').value);
+        const optionAmount = parseFloat(document.getElementById('amount').value);
         const optionElement = document.getElementById('list-account');
         const optionName = optionElement.options[optionElement.selectedIndex];
-        const acc = new BankAccount(optionName, optionAmount);
-        acc.deposit(optionAmount);
-        
-       // this.deposit(amount_deposit);
-
+        const acc = BankAccount.accountInfoList.filter((acc) => {
+            if(acc._name == optionName.innerHTML){
+                return acc;
+            }
+        });
+        acc[0].deposit(optionAmount);
+        this.renderAccounts()
       }
 
      static debit(amount) {
@@ -73,17 +105,64 @@ class BankAccount{
           alert('Insufficient funds');
         }
       }
-
 }
+function desabledBtn(elem){
+  if(elem !== '') 
+  document.getElementById('btn-sumit').disabled = false;
+  document.getElementById('btn-submit-depit').disabled = false;
+}
+
+//document.getElementById('btn-sumit').disabled = true;
 
 document.getElementById('btn-account').addEventListener('click', () => {
     BankAccount.createAccount();
   });
 
-  document.getElementById('btn-deposit').addEventListener('click', () => {
+  document.getElementById('btn-sumit').addEventListener('click', () => {
     BankAccount.operation_deposit();
+    toggleView("mainView")
+    // Hide the div
+  });
+
+  document.getElementById('btn-submit-depit').addEventListener('click', () => {
+    BankAccount.operation_depit();
+    toggleView("mainView")
+    // Hide the div
+  });
+
+  document.getElementById('btn-display').addEventListener('click', () => {
+    toggleView("depositView")
+    //BankAccount.operation_deposit();
+    // Display the div
   });
 
   document.getElementById('btn-debit').addEventListener('click', () => {
-    BankAccount.debit();
+    console.log("here");
+    toggleView("depitView")
   });
+
+  function toggleView(viewName){
+    const mainView = document.querySelector("#mainView");
+    const depositView = document.querySelector("#depositView");
+    const depitView = document.querySelector("#depitView");
+    switch(viewName){
+        case "mainView":{
+            mainView.classList.remove("hidden")
+            depositView.classList.add("hidden")
+            depitView.classList.add("hidden")
+            break;
+        }
+        case "depositView":{
+            mainView.classList.add("hidden")
+            depitView.classList.add("hidden")
+            depositView.classList.remove("hidden")
+            break
+        }
+        case "depitView":{
+            mainView.classList.add("hidden")
+            depitView.classList.remove("hidden")
+            depositView.classList.add("hidden")
+            break;
+        }
+    }
+  }
