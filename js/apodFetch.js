26733d0fc999d352d4f06ecb54c5c6a36660571a
Raimundo Pereira <raipereira84@gmail.com>
2023-05-22 10:43:43 -0500

    edit apodFetch js

--- js/apodFetch.js
+++ js/apodFetch.js
@@ -2,13 +2,21 @@ $(document).ready(
     function () {
         $("#view_button").click(getPicture);
     });
-const params = new URLSearchParams();
-params.append('api_key', 'Ar0slwQxekBVpMi4fFtXmooqdthgEOEvQ94OdREI');
-params.append('date', $("#date").val());
-const url = `https://api.nasa.gov/planetary/apod${params.toString}`;
+
 
 function getPicture() {
+    const apiKey = 'Ar0slwQxekBVpMi4fFtXmooqdthgEOEvQ94OdREI';
+    const params = new URLSearchParams();
+    params.append('api_key', apiKey);
+    params.append('date', $("#date").val());
+    const url = `https://api.nasa.gov/planetary/apod?${params.toString()}`;
     fetch(url)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Error: ' + response.status);
+        }
+        return response.json();
+      })
         .then(showPicture)
         .catch(noPicture)
 };
